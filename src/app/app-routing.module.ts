c6a82035dfc11fbab65cf623f7709c220f8aea66
spokade@gmail.com <spokade@gmail.com>
2019-02-02 23:56:42 +0530

    Add Component and Initial Html.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'add', component:AddComponent},
+  {path:'edit/:id', component:EditComponent},
+  {path:'**',  redirectTo:'/home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
