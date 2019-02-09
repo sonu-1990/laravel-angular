7d2dbc1e1570b804da4d7fe3b69f54f970224461
spokade@gmail.com <spokade@gmail.com>
2019-02-09 23:11:44 +0530

    Add header to http call.

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -3,7 +3,6 @@ import { HttpHeaders } from '@angular/common/http';
 import { HttpClient }    from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
-import { Headers,RequestOptions } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +10,17 @@ import { Headers,RequestOptions } from '@angular/http';
 export class ProfileService {
   // local Laravel server
   server = 'http://laravel-angular.test/';
-  headers:Headers = new Headers();
+  headers:HttpHeaders = new HttpHeaders();
   options: any;
   constructor(private http:HttpClient) { 
-    this.headers.append({
-      'Content-Type':'application/json',
-      'enctype':'multipart/form-data',
-      'X-requested-With':'XMLHttpRequest'
-    });
-    this.options = new RequestOptions({headers:this.headers});
+    this.headers.append(
+      'Content-Type','application/json'
+    );
+    this.headers.append(
+      'enctype','multipart/form-data'
+    );
+    this.headers.append(
+      'X-requested-With','XMLHttpRequest'
+    );
   }
 }
